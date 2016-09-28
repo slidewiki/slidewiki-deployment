81d8b111f8fac4affea424a9733ab7d2c5a0a2c3
Roy Meissner <roy-meissner@gmx.net>
2016-09-28 16:53:06 +0200

    Added pull first

--- docker-compose/start.sh
+++ docker-compose/start.sh
@@ -8,6 +8,7 @@ if [ $# -lt 1 ] ; then
 fi
 
 chown -R 8983:8983 solrserver-config/solr-*
+docker-compose -f $1 pull
 docker-compose -f $1 up -d
 sleep 10s
 docker exec $(docker ps -f name=mongodb* -q) mongo --eval 'config = { _id : "rs0", "members" : [ {_id : 0, "host" : "localhost:27017"} ] }; rs.initiate(config);'
