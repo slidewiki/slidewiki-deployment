c631a4da708a721857306901cfde647df580b4a1
Roy Meissner <roy-meissner@gmx.net>
2016-09-30 12:48:09 +0200

    Added solr fs access rights fix

--- docker-compose/prepare.sh
+++ docker-compose/prepare.sh
@@ -12,3 +12,4 @@ chown -R 8983:8983 solrserver-config/solr-*
 docker-compose -f $1 up -d mongodb
 sleep 10s
 docker exec $(docker ps -f name=mongodb* -q) mongo --eval 'config = { _id : "rs0", "members" : [ {_id : 0, "host" : "localhost:27017"} ] }; rs.initiate(config);'
+docker exec mongodb chown -R 8983:8983 /solr-data/
