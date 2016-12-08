be056582d8c5c9b93f5376d41960f5ad3261e975
Roy Meissner <meissner@informatik.uni-leipzig.de>
2016-12-08 16:31:32 +0100

    Added a all in one maintenance container

--- backup_scripts/backup_database.sh
+++ backup_scripts/backup_database.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-TEMP_PATH=/tmp/backup
-VOLUME_PATH_DB=/data/docker/volumes/database/_data/dump
-CONTAINER_PATH_DB=/data/db/dump
-VOLUME_PATH_FILES=/data/docker/volumes/files/
 BACKUP_DATE=$(date +%F)
 
-mkdir -p $TEMP_PATH/db
-mkdir -p $TEMP_PATH/files
- 
+mkdir -p /data/maintenance/db
+mkdir -p /data/maintenance/files
 
 echo "Creating dump from slidewiki database"
 echo ""
-docker exec mongodb mongodump -o $CONTAINER_PATH_DB/slidewiki
-tar --gzip -cvf $TEMP_PATH/db/slidewiki.mongodump.$BACKUP_DATE.tar.gz -C $VOLUME_PATH_DB/slidewiki/ slidewiki
-mv $TEMP_PATH/db/slidewiki.mongodump.$BACKUP_DATE.tar.gz /data/maintenance/db
+mongodump --host mongodb:27017 --db slidewiki --gzip --archive /maintenance/db/slidewiki.mongodump.$BACKUP_DATE.archive.gz
+#tar --gzip -cvf /data/maintenance/db/slidewiki.mongodump.$BACKUP_DATE.tar.gz -C /tmp/slidewiki/ slidewiki
 echo ""
 
 echo "Backing up files"
 echo ""
-tar -cvf $TEMP_PATH/files/files.$BACKUP_DATE.tar -C $VOLUME_PATH_FILES _data
-rm -f /data/maintenance/files/*
-mv $TEMP_PATH/files/files.$BACKUP_DATE.tar /data/maintenance/files
+tar -cvf /maintenance/files/files.$BACKUP_DATE.tar -C /data files
