3078ddf1cd35cdf779f84b9323490ddb6ab5761a
Roy Meissner <meissner@informatik.uni-leipzig.de>
2016-12-09 10:59:45 +0100

    Improved backup script

--- maintenance/backup_database.sh
+++ maintenance/backup_database.sh
@@ -5,12 +5,11 @@ BACKUP_DATE=$(date +%F)
 mkdir -p /maintenance/db
 mkdir -p /maintenance/files
 
-echo "Creating dump from slidewiki database" >> /var/log/cron.log
-echo "" >> /var/log/cron.log
-mongodump --host mongodb:27017 --db slidewiki --gzip --archive=/maintenance/db/slidewiki.mongodump.$BACKUP_DATE.archive.gz >> /var/log/cron.log
-#tar --gzip -cvf /data/maintenance/db/slidewiki.mongodump.$BACKUP_DATE.tar.gz -C /tmp/slidewiki/ slidewiki
-echo "" >> /var/log/cron.log
+echo "Creating dump from slidewiki database" >> /var/log/cron.log 2>&1
+echo "" >> /var/log/cron.log 2>&1
+mongodump --host mongodb:27017 --db slidewiki --gzip --archive=/maintenance/db/slidewiki.mongodump.$BACKUP_DATE.archive.gz >> /var/log/cron.log 2>&1
+echo "" >> /var/log/cron.log 2>&1
 
-echo "Backing up files" >> /var/log/cron.log
-echo "" >> /var/log/cron.log
-tar -cvf /maintenance/files/files.$BACKUP_DATE.tar -C /data files >> /var/log/cron.log
+echo "Backing up files" >> /var/log/cron.log 2>&1
+echo "" >> /var/log/cron.log 2>&1
+tar --gzip -cvf /maintenance/files/files.$BACKUP_DATE.tar.gz -C /data files >> /var/log/cron.log 2>&1
