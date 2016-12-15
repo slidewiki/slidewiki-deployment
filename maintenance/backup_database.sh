0ae617befc8a5947d25b92b4f0014cab7f962be9
Roy Meissner <meissner@informatik.uni-leipzig.de>
2016-12-15 10:34:13 +0100

    improved backup script

--- maintenance/backup_database.sh
+++ maintenance/backup_database.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 BACKUP_DATE=$(date +%F)
+TO_REMOVE=$(date --date="-5 days" +'%F')
 
 mkdir -p /maintenance/db
 mkdir -p /maintenance/files
 
-echo "Creating dump from slidewiki database" >> /var/log/cron.log 2>&1
-echo "" >> /var/log/cron.log 2>&1
-mongodump --host mongodb:27017 --db slidewiki --gzip --archive=/maintenance/db/slidewiki.mongodump.$BACKUP_DATE.archive.gz >> /var/log/cron.log 2>&1
-echo "" >> /var/log/cron.log 2>&1
+echo "Creating dump from slidewiki database"
+echo ""
+mongodump --host mongodb:27017 --db slidewiki --gzip --archive=/maintenance/db/slidewiki.mongodump.$BACKUP_DATE.archive.gz
+echo ""
 
-echo "Backing up files" >> /var/log/cron.log 2>&1
-echo "" >> /var/log/cron.log 2>&1
-TOREMOVE=$(date --date="-5 days" +'%F')
-rm -f /maintenance/files/files.$TOREMOVE.tar.gz
-tar --gzip -cvf /maintenance/files/files.$BACKUP_DATE.tar.gz -C /data files >> /var/log/cron.log 2>&1
+echo "Backing up files"
+echo ""
+rm -f /maintenance/files/files.$TO_REMOVE.tar.gz
+tar --gzip -cvf /maintenance/files/files.$BACKUP_DATE.tar.gz -C /data files
