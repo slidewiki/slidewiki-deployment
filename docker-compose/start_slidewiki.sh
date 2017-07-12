c591b37461edf65c6833735074b156a8567a756c
root <root@test-slidewiki.tib.eu>
2017-07-12 21:47:22 +0200

    fixed wrong yml file name in start script and moved prepare.sh functionality into start script

--- docker-compose/start_slidewiki.sh
+++ docker-compose/start_slidewiki.sh
@@ -2,8 +2,9 @@
 
 PRJ=$1
 
-docker-compose -p $PRJ -f docker-compose.yml -f docker-compose.testing.https.yml  up -d logger
-docker-compose -p $PRJ -f docker-compose.yml -f docker-compose.testing.https.yml  up -d mongodb
-docker-compose -p $PRJ -f docker-compose.yml -f docker-compose.testing.https.yml  up -d solr
-sleep 2
-docker-compose -p $PRJ -f docker-compose.yml -f docker-compose.testing.https.yml  up -d
+docker-compose -p $PRJ -f docker-compose.yml -f docker-compose.production.https.yml  up -d logger
+sleep 1
+docker-compose -p $PRJ -f docker-compose.yml -f docker-compose.production.https.yml  up -d mongodb solr
+sleep 3
+docker exec $(docker ps -f name=mongodb* -q) mongo --eval 'config = { _id : "rs0", "members" : [ {_id : 0, "host" : "localhost:27017"} ] }; rs.initiate(config);'
+docker-compose -p $PRJ -f docker-compose.yml -f docker-compose.production.https.yml  up -d
