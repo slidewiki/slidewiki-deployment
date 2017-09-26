bb119bdcc6069647ba58296ad87249e72b585507
Ben Wulff <Benjamin.Wulff@iais.fraunhofer.de>
2017-09-26 10:06:07 +0200

    backup restore script now functional

--- docker-compose/backup_restore.sh
+++ docker-compose/backup_restore.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-SOURCE_URL=$1
-BACKUPDATE_DATE=$2
-INSTANCE_NAME=$(cat instance_name)
-
-# pull latest database dump from stable
-curl -u dev:killywilly https://maintenance.$SOURCE_URL/db/slidewiki.mongodump.$BACKUP_DATE.archive.gz > /data/docker/volumes/$INSTANCE_NAME\_mongo-data/_data/dump/mongodump.archive.gz 
-
-# load database dump
+cp 1 > /data/docker/volumes/$INSTANCE_NAME\_mongo-data/_data/dump/mongodump.archive.gz
 docker exec -it $INSTANCE_NAME\_mongodb_1 mongorestore --drop --gzip --archive=/data/db/dump/mongodump.archive.gz
-
-# clean up
 rm /data/docker/volumes/$INSTANCE_NAME\_mongo-data/_data/dump/mongodump.archive.gz
-
